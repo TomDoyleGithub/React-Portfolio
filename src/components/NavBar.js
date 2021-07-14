e57e140639701eeb16ab67cb803795cd3fa1d367
Punkinut <heypunkinut@gmail.com>
2021-07-14 15:01:31 +1000

    Imported original CSS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 function Nav() {
-  const message = 'Hello world! I am a React Component';
 
   return (
     <div className="container">
-      <h2>{message}</h2>
+      <p>Welcome</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/reset.css'
+import './styles/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
