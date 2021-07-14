e92ea6e88833148efea10a6bc1fe08043d74dbc1
Punkinut <heypunkinut@gmail.com>
2021-07-14 14:45:00 +1000

    Added basic file structure

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+  const message = 'Hello world! I am a React Component';
+
+  return (
+    <div className="container">
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default Nav;
