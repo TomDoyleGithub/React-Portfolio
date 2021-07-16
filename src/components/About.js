8b29e15299403e1834acf2c1fc507ad9d33e0541
Punkinut <heypunkinut@gmail.com>
2021-07-16 14:56:22 +1000

    Added fade in to about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 import { Link } from "react-scroll";
 import proPic from '../images/pro-pic.jpg'
 import speeachArrow from '../images/speech-arrow.svg'
@@ -8,6 +9,7 @@ function About() {
     return (
         <section id="intro-background">
             <section className="intro-grid">
+                <Fade bottom>
                 <img src={proPic} className="pro-pic" alt="Profile Icon"/>
                 <article>
                     <img src={speeachArrow} className="arrow" alt="Speach Arrow"/>
@@ -16,6 +18,7 @@ function About() {
                     <p>Well I'm a full stack developer, based in the heart of Sydney! My goal is to make sure your brand meets todays modern standards.</p>
                     <Link activeClass="active" to="skills-background" spy={true} smooth={true} offset={-70} duration={400}><button><img src={book} className="book" alt="book icon"/>Learn More</button></Link>
                 </article>
+                </Fade>
             </section>
         </section>
     )
