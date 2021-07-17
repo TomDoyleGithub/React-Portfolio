3e9de5280d626eff93ac602a66573607b4e1bdf4
Punkinut <heypunkinut@gmail.com>
2021-07-17 13:40:34 +1000

    Fixed speed of navigation scroll

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -14,8 +14,8 @@ return (
             <img src={close} className="cross" alt="Close Button" onClick={hideNav}/>
             <Link activeClass="active" to="header-grid" spy={true} smooth={true} offset={0} duration={400} onClick={hideNav}>Home.</Link>
             <Link activeClass="active" to="intro-background" spy={true} smooth={true} offset={0} duration={1000} onClick={hideNav}>About Me.</Link>
-            <Link activeClass="active" to="works-grid" spy={true} smooth={true} offset={-70} duration={400} onClick={hideNav}>Works.</Link>
-            <Link activeClass="active" to="contact-background" spy={true} smooth={true} offset={-70} duration={400} onClick={hideNav}>Contact.</Link>
+            <Link activeClass="active" to="works-grid" spy={true} smooth={true} offset={-70} duration={1200} onClick={hideNav}>Works.</Link>
+            <Link activeClass="active" to="contact-background" spy={true} smooth={true} offset={-70} duration={1300} onClick={hideNav}>Contact.</Link>
         </section>
     </section>
 )
