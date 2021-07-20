fa2dc8bcc246e600fb61aaab018e1b4c8debc97c
Punkinut <heypunkinut@gmail.com>
2021-07-20 10:11:26 +1000

    Updated resume link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ function Contact() {
                     </a>
                 </article>
                 <section className="resume">
-                    <a href="https://drive.google.com/file/d/1HMwQBFhpEKrhULFgEYteXw8zN9-RBdE4/view?usp=sharing" target="_blank" rel="noreferrer"><button className="resume-button">Resume</button></a>
+                    <a href="https://drive.google.com/file/d/1Ec-mxwteIAZnHQZyx9zt-jHQDVatDm9I/view?usp=sharing" target="_blank" rel="noreferrer"><button className="resume-button">Resume</button></a>
                 </section>
             </section>
         </section>  
